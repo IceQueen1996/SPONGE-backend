df854019b8d643d7dffd2112598fa5a43a676e9e
Elisabeth Pachl <pachl@cip.ifi.lmu.de>
2019-10-27 12:19:36 +0100

    change database url to environment variable

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ CORS(app)
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:a6/4qpQYx]QA@10.162.163.20/sponge"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SPONGE_DB_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
