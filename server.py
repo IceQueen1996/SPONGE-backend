267fb042554e2a4868f89fe9b69ca450ffa1067a
Elisabeth Pachl <pachl@cip.ifi.lmu.de>
2020-03-05 15:23:59 +0100

    change server url

--- server.py
+++ server.py
@@ -13,6 +13,11 @@ connex_app = config.connex_app
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api("swagger.yml")
 
+# create a URL route in our application for "/"
+@connex_app.route("/")
+def home():
+    return None
+
 cli = sys.modules['flask.cli']
 cli.show_server_banner = lambda *x: None
 
