040b9d08879a771bfaafecff00c93168413d0efb
Elisabeth Pachl <pachl@cip.ifi.lmu.de>
2020-03-05 13:36:45 +0100

    add method to search for single interaction partner in ​/miRNAInteraction​/findceRNA

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 """
 Main module of the server file
 """
+import sys
+import os
 
 # local modules
 import config
@@ -12,9 +14,13 @@ connex_app = config.connex_app
 connex_app.add_api("swagger.yml")
 
 # create a URL route in our application for "/"
-@connex_app.route("/")
+@connex_app.route(os.getenv("SPONGE_API_URI"))
 def home():
     return None
 
+cli = sys.modules['flask.cli']
+cli.show_server_banner = lambda *x: None
+
+
 if __name__ == "__main__":
-    connex_app.run(debug=False)
+    connex_app.run()
