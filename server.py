00b47b9cf38757c5d30e14509ec35882187d88d9
Elisabeth Pachl <pachl@cip.ifi.lmu.de>
2020-03-13 10:44:38 +0100

    try to set base url to env

--- server.py
+++ server.py
@@ -1,24 +1,19 @@
 """
 Main module of the server file
 """
-#import os
-#from swagger_modifier import replaceAll
-#replaceAll("swagger.yml", "- url", "- url: " + os.getenv("SPONGE_API_URL"))
+import os
+from swagger_modifier import replaceAll
 
 # local modules
 import config
 
-# Get the application instance
-connex_app = config.connex_app
-
-# Read the swagger.yml file to configure the endpoints
-connex_app.add_api("swagger.yml")
-
-# create a URL route in our application for "/"
-@connex_app.route("/")
-def home():
-    return None
-
 if __name__ == "__main__":
+    replaceAll("swagger.yml", "- url", "- url: " + os.getenv("SPONGE_API_URL"))
+
+    # Get the application instance
+    connex_app = config.connex_app
+
+    # Read the swagger.yml file to configure the endpoints
+    connex_app.add_api("swagger.yml")
     connex_app.run()
 
